3e2d86df8190e2e45524f15c1b538b677b4fcbf8
axe312ger <opensource@axe312.dev>
2021-05-23 13:43:40 +0200

    rewrite to dummy output so we can add rsync later

---
+++ src/cache.ts
@@ -0,0 +1,75 @@
+import {DownloadOptions, UploadOptions} from './options'
+
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'ValidationError'
+    Object.setPrototypeOf(this, ValidationError.prototype)
+  }
+}
+
+export class ReserveCacheError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'ReserveCacheError'
+    Object.setPrototypeOf(this, ReserveCacheError.prototype)
+  }
+}
+
+function checkPaths(paths: string[]): void {
+  if (!paths || paths.length === 0) {
+    throw new ValidationError(
+      `Path Validation Error: At least one directory or file path is required`
+    )
+  }
+}
+
+function checkKey(key: string): void {
+  if (key.length > 512) {
+    throw new ValidationError(
+      `Key Validation Error: ${key} cannot be larger than 512 characters.`
+    )
+  }
+  const regex = /^[^,]*$/
+  if (!regex.test(key)) {
+    throw new ValidationError(
+      `Key Validation Error: ${key} cannot contain commas.`
+    )
+  }
+}
+
+/**
+ * Restores cache from keys
+ *
+ * @param paths a list of file paths to restore from the cache
+ * @param primaryKey an explicit key for restoring the cache
+ * @param restoreKeys an optional ordered list of keys to use for restoring the cache if no cache hit occurred for key
+ * @param downloadOptions cache download options
+ * @returns string returns the key for the cache hit, otherwise returns undefined
+ */
+export async function restoreCache(
+  paths: string[],
+  primaryKey: string,
+  restoreKeys?: string[],
+  options?: DownloadOptions
+): Promise<string | undefined> {
+  console.log(JSON.stringify({paths, primaryKey, restoreKeys, options}))
+  return Promise.resolve('wohow')
+}
+
+/**
+ * Saves a list of files with the specified key
+ *
+ * @param paths a list of file paths to be cached
+ * @param key an explicit key for restoring the cache
+ * @param options cache upload options
+ * @returns number returns cacheId if the cache was saved successfully and throws an error if save fails
+ */
+export async function saveCache(
+  paths: string[],
+  key: string,
+  options?: UploadOptions
+): Promise<number> {
+    console.log(JSON.stringify({paths, key, options}))
+    return Promise.resolve(420)
+}
